4a9dfe201f6015e082e876867b5851d2a09850b0
tt7182 <64065992+tt7182@users.noreply.github.com>
2021-03-03 19:02:14 +0000

    debug

--- apifunctions.php
+++ apifunctions.php
@@ -113,21 +113,12 @@
         $allSeasonsAPIurl = "http://tkilpatrick01.lampt.eeecs.qub.ac.uk/epl_api_v1/list?all_seasons_list";
         $allSeasonsAPIdata = file_get_contents($allSeasonsAPIurl);
         $seasonList = json_decode($allSeasonsAPIdata, true);
-
-        if ($seasonList->num_rows > 0) {
-            $dbHighestSeasonEntered = $seasonList->fetch_row();
-        }
-
-        print_r($dbHighestSeasonEntered);
+        $dbHighestSeasonEntered = $seasonList[0]["season"];
 
         $seasonYearsArray = explode("-", $dbHighestSeasonEntered);
-        print_r($seasonYearsArray);
         $seasonEndYear = (int) $seasonYearsArray[1];
-        echo "<p>{$seasonEndYear}</p>";
         $nextSeasonEndYear = $seasonEndYear + 1;
-        echo "<p>{$nextSeasonEndYear}</p>";
-        // return "{$seasonEndYear}-{$nextSeasonEndYear}";
-        echo "{$seasonEndYear}-{$nextSeasonEndYear}";
+        return "{$seasonEndYear}-{$nextSeasonEndYear}";
     }
 
     // take a users Referee Name entry and parse into a format required for the database
